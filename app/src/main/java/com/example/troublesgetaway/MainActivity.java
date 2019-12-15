505b4576344656d05c6b286acdf760ddbb2e4f9b
SamiAtu <samithatu@gmail.com>
2019-12-15 18:41:32 +0100

    Aggiunta connessione db

--- app/src/main/java/com/example/troublesgetaway/MainActivity.java
+++ app/src/main/java/com/example/troublesgetaway/MainActivity.java
@@ -2,19 +2,61 @@ package com.example.troublesgetaway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usernameTxt, passwordTxt;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameTxt = findViewById(R.id.username);
+        passwordTxt = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameTxt.getText().toString();
+                String password = passwordTxt.getText().toString();
+                if (!TextUtils.isEmpty(username.trim()) && !TextUtils.isEmpty(password.trim())) {
+                    tryLogin(username, password);
+                } else {
+                    //l'utente non ha riempito entrambi i campi
+                }
+            }
+        });
     }
 
-    void tryLogin(View view) {
+    private void tryLogin(String username, String password) {
+        MyApiService apiService = RetrofitService.getInstance();
+        apiService.login(username, password).enqueue(new Callback<List<Void>>() {
+            @Override
+            public void onResponse(Call<List<Void>> call, Response<List<Void>> response) {
+                //risposta affermativa
+                if (response.isSuccessful()) {
+                    //vai avanti
+                }
+            }
 
-
+            @Override
+            public void onFailure(Call<List<Void>> call, Throwable t) {
+                //risposta connessione
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/troublesgetaway/RetrofitService.java
@@ -0,0 +1,36 @@
+package com.example.troublesgetaway;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public class RetrofitService {
+
+    private static final String BASE_URL = "http://troublegetawaydb.altervista.org/";
+
+    private static MyApiService service = null;
+
+    public static MyApiService getInstance() {
+        if (service == null) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(BASE_URL)
+                    .build();
+            service = retrofit.create(MyApiService.class);
+        }
+        return service;
+    }
+}
+
+interface MyApiService {
+
+    @FormUrlEncoded
+    @POST("login.php")
+    Call<List<Void>> login(@Field("usr") String username, @Field("psw") String password);
+
+}
